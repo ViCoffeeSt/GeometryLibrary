6f0692ed6d1a1f371dbbf4316e067c6abc15cfb1
ViCoffeeSt <127383715+ViCoffeeSt@users.noreply.github.com>
2023-04-22 15:02:24 +0300

    Create test_sql.sql

---
+++ SQL/test_sql.sql
@@ -0,0 +1,6 @@
+SELECT Products.ProductName, Categories.CategoryName
+FROM Products
+LEFT OUTER JOIN Product_Category
+ON Products.ProductID = Product_Category.ProductID
+LEFT OUTER JOIN Categories
+ON Product_Category.CategoryID = Categories.CategoryID
